1a29ec6615736e873a77a1152c3c4fa9ef8ae803
Elliott Mina <elliottmina@gmail.com>
2019-12-14 16:02:42 -0800

    Fixing date inclusion in CSV row.

--- run.py
+++ run.py
@@ -12,8 +12,8 @@ apikey = os.getenv('darksky_api_key')
 lat_long = os.getenv('lat_long')
 csv_name = os.getenv('csv_name')
 
-start_date = date(2019, 1, 1)
-finish_date = date(2019, 12, 14)
+start_date = date(2009, 1, 1)
+finish_date = date(2018, 1, 1)
 
 expected_keys = 'apparentTemperatureHigh','apparentTemperatureHighTime','apparentTemperatureLow','apparentTemperatureLowTime','apparentTemperatureMax','apparentTemperatureMaxTime','apparentTemperatureMin','apparentTemperatureMinTime','cloudCover','dewPoint','humidity','icon','moonPhase','ozone','precipIntensity','precipIntensityMax','precipIntensityMaxTime','precipProbability','precipType','pressure','summary','sunriseTime','sunsetTime','temperatureHigh','temperatureHighTime','temperatureLow','temperatureLowTime','temperatureMax','temperatureMaxTime','temperatureMin','temperatureMinTime','time','uvIndex','uvIndexTime','visibility','windBearing','windGust','windGustTime','windSpeed'
 
@@ -48,16 +48,13 @@ def fetch(datestr):
   url = f'https://api.darksky.net/forecast/{apikey}/{lat_long},{time}?exclude=currently,minutely,hourly,alerts,flags'
   return requests.get(url)
 
-def write(response, writer):
+def write(datestr, response, writer):
   data = response.json()
   values = extract_values(data['daily']['data'][0])
-  writer.writerow(values)
+  writer.writerow([datestr,] + values)
 
 def extract_values(dict):
-  values = [datestr,]
-  for key in expected_keys:
-    values.append(dict.get(key))
-  return values
+  return [dict.get(key) for key in expected_keys]
 
 if __name__== "__main__":
   main()
